9b7c84b0c3aa868bab22b60ed348699e8ec975aa
Om Nigam <om2021064@akgec.ac.in>
2023-09-29 10:05:01 +0530

    first Commit

---
+++ BackEnd/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const userRouter = require("./routes/user.js");
+const adminRouter = require("./routes/Admin.js");
+const { authenticateUser } = require("./middlewares/auth.js");
+const { Courses } = require("./database/database.js");
+//Other logics
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Coursera");
+});
+
+//Single Course Id
+app.get("/courses/:courseId", authenticateUser, async (req, res) => {
+  const courseId = req.params.courseId;
+  const course = await Courses.findById(courseId);
+
+  if (course) {
+    res.status(200).json(course);
+  } else {
+    res.status(404).json({ message: "Course not found" });
+  }
+});
+
+//Connection
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "mernCourse",
+  })
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database " + err);
+  });
+
+//Hosting configuration
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
+});
